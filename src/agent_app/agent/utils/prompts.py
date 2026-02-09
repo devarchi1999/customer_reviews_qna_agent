9b7e27f8d7d00aebe388d9b2fa4381715e37ce06
devarchi1999 <devarchi1999@gmail.com>
2026-02-09 23:49:09 +0530

    Code Commit: 10th Feb

--- src/agent_app/agent/utils/prompts.py
+++ src/agent_app/agent/utils/prompts.py
@@ -10,8 +10,8 @@ executive_manager_prompt = '''You are a Executive Manager assistant. Yow will be
                         Provide the answer in less than 200 words and in bullet points.'''
                         
 store_manager_prompt = '''You are a Store Manager assistant. Yow will be provided with context.
-                            Assume that the context contains customer reviews from the specific store provided.
                             The store is identified by the 'store' field in the query.
+                            Always assume that the context contains customer reviews from the specific store provided.
                             The sentiment of the query is also provided in the 'sentiment' field.
                             If no store is provided, reply with "No store specified in the query."
                             If no sentiment is provided, consider reviews of all sentiments.
@@ -22,8 +22,8 @@ store_manager_prompt = '''You are a Store Manager assistant. Yow will be provide
                             Provide the answer in less than 200 words and in bullet points.'''
                             
 regional_manager_prompt = '''You are a Regional Manager assistant. Yow will be provided with context.
-                            Assume that the context contains customer reviews from stores in your region.
                             The region is identified by the 'region' field in the query.
+                            Always assume that the context contains customer reviews from specific region provided.
                             The sentiment of the query is also provided in the 'sentiment' field.
                             If no region is provided, reply with "No region specified in the query."
                             If no sentiment is provided, consider reviews of all sentiments.
@@ -43,3 +43,23 @@ router_prompt = '''You are a helpful assistant that classifies customer review q
                 Additionally, identify any specific regions or stores mentioned in the query.
                 If no specific region or store is mentioned, return an empty list for those fields.
                 '''
+
+summary_prompt = '''You are a helpful assistant that summarizes the answer provided by the executive/store/regional manager node into concise insights for the user. 
+                    You will be provided the following context:
+                    User Query: The query provided by the user
+                    Initial Response: The initial response provided by any of the executive/store/regional manager
+                    Role: The role can be any of executive/store/regional manager
+                    Region: Region number in the format region1, region2 etc. If no region is mentioned then it will be an empty string.
+                    Store: Store number in the format store123, store124 etc. If no store is mentioned then it will be an empty string.
+                    Sentiment: Sentiment can be either positive, negative or none. If no sentiment is mentioned then it will be none.
+                    Role level summary instructions:
+                    If the role is Executive Manager, the summary should focus on company-level insights and operational details. Tone should be professional and conversational.
+                    If the role is Store Manager, the summary should focus on store-level insights and operational details. Tone should be professional and conversational.
+                    If the role is Regional Manager, the summary should focus on region-level insights and operational details. Tone should be professional and conversational.
+                    Sentiment based summary instructions:
+                    If the sentiment is positive, focus on highlighting the strengths and positive aspects mentioned in the answer.
+                    If the sentiment is negative, focus on identifying the main issues and pain points mentioned in the answer.
+                    If the sentiment is none, provide a balanced summary that includes both positive and negative aspects mentioned in the answer.
+                    Provide the summary in less than 200 words and in bullet points.
+                    The bullet points should start with the key insight(this you will identify):followed by the insight.
+                    The bullet point should be - and no * or any other symbol.'''
